265e01564c5b03d332750c5b45d0fbc925798d2e
ibabaityte <47790653+ibabaityte@users.noreply.github.com>
2021-12-07 19:13:00 +0200

    Added style to the admin subscribers list component

--- client/src/components/admin/subscribers/AdminSubscribers.js
+++ client/src/components/admin/subscribers/AdminSubscribers.js
@@ -3,29 +3,36 @@ import React, {useEffect, useState} from "react";
 // util imports
 import {fetchSubscribers} from "../../../utils/admin/adminSubscriberUtils";
 
-const AdminSubscribers = () => {
+// style imports
+import {withStyles} from "@material-ui/core/styles";
+import adminSubscriberStyle from "../../../utils/style/adminSubscriberStyle";
+
+const AdminSubscribers = (props) => {
+
+    const classes = props.classes;
 
     const [subscribers, setSubscribers] = useState([]);
 
     useEffect(() => {
-         fetchSubscribers(setSubscribers);
+        fetchSubscribers(setSubscribers);
     }, []);
 
     return (
         <div>
-            <div>Admin Subscribers</div>
-            {
-                subscribers.map((sub, key) => {
-                    return (
-                        <div key={key}>
-                            <div>{key + 1}</div>
-                            <div>{sub.email}</div>
-                        </div>
-                    );
-                })
-            }
+            <h1 className={classes.heading}>Admin Subscribers</h1>
+            <div className={classes.grid}>
+                {
+                    subscribers.map((sub, key) => {
+                        return (
+                            <div key={key} className={classes.emailWrapper}>
+                                <div className={classes.email}><span className={classes.emailNumber}>{key + 1}.</span> {sub.email}</div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 };
 
-export default AdminSubscribers;
+export default withStyles(adminSubscriberStyle)(AdminSubscribers);
---
+++ client/src/utils/style/adminSubscriberStyle.js
@@ -0,0 +1,32 @@
+const adminSubscriberStyle = (theme) => ({
+    grid: {
+        width: "30%",
+        [theme.breakpoints.down('sm')]: {
+            width: "50%",
+        },
+        margin: "10px auto 50px auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        boxShadow: "0px 0px 33px -11px black",
+        backgroundColor: "rgb(240,240,240)",
+        padding: "80px"
+    },
+    heading: {
+        textAlign: "center",
+        margin: "50px"
+    },
+    emailWrapper: {
+        textAlign: "center",
+        margin: "auto"
+    },
+    email: {
+        margin: "15px auto",
+    },
+    emailNumber: {
+        fontWeight: "bold",
+        margin: "5px"
+    }
+});
+
+export default adminSubscriberStyle;
